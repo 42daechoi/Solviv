7903946fae4e87bf80ce1c093a7fab4fb879ac83
42 <daechoi@student.42seoul.kr>
2024-10-05 18:20:28 +0900

    SOL-15 <NetworkManager - 방 생성, 방 입장>

---
+++ Assets/Scripts/UnloadManagers/NetworkManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        // 포톤 서버 연결
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("포톤 마스터 서버 접속 완료");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("로비 접속 완료");
+        // 방 생성 또는 입장에 필요한 준비 코드 필요
+    }
+
+    public void CreateRoom()
+    {
+        RoomOptions roomOptions = new RoomOptions { MaxPlayers = 4 };
+        PhotonNetwork.CreateRoom("RoomName", roomOptions);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("방 생성 완료");
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom("RoomName");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("방 입장 완료");
+        // 플레이어 캐릭터 인스턴스화 과정 필요
+        // PhotonNetwork.Instantiate(playerPrefab.name, Vector3.zero, Quaternion.identity, 0);
+    }
+
+    //public override void OnPlayerLeftRoom(Player otherPlayer)
+    //{
+    //    Debug.Log(otherPlayer.NickName + "님이 퇴장하셨습니다.");
+    //}
+
+}
