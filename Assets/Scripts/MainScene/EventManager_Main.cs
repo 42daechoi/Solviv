76d6415d2519492c553641bee6ebf9d12ff6e5d7
zuhwan0205 <ljh20230379@gmail.com>
2024-10-07 18:24:13 +0900

    SOL-20<Option - 설정 종류 선택 패널 제작>

--- Assets/Scripts/MainScene/EventManager_Main.cs
+++ Assets/Scripts/MainScene/EventManager_Main.cs
@@ -13,6 +13,10 @@ public class EventManager_Main : MonoBehaviour
     public static event Action OnQuitClicked;
     public static event Action OnSoloModeClicked;
     public static event Action OnMultiModeClicked;
+    public static event Action OnControlPanelButtonClicked;
+    public static event Action OnResolutionPanelButtonClicked;
+    public static event Action OnAudioPanelButtonClicked;
+    public static event Action OnOptionConfirmButtonClicked;
 
      public void OnClickFindGame() {
         OnFindGameClicked?.Invoke();   
@@ -27,6 +31,23 @@ public class EventManager_Main : MonoBehaviour
     public void OnClickOption(){
         OnOptionClicked?.Invoke();
     }
+    public void OnClickControlPanelButton(){
+        OnControlPanelButtonClicked?.Invoke();
+        Debug.Log("컨트롤 패널");
+    }
+    public void OnClickResolutionPanelButton(){
+        OnResolutionPanelButtonClicked?.Invoke();
+        Debug.Log("해상도 패널");
+    }
+    public void OnClickAudioPanelButton(){
+        OnAudioPanelButtonClicked?.Invoke();
+        Debug.Log("오디오 패널");
+    }
+    public void OnOptionConfirmButton(){
+        OnOptionConfirmButtonClicked?.Invoke();
+        Debug.Log("설정 적용");
+    }
+
     public void OnClickQuit(){
         OnQuitClicked?.Invoke();
         
