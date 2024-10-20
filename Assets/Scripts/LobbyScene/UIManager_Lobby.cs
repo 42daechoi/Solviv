be0656d909b5e75670d68b0060267d77f60d20b3
42 <daechoi@student.42seoul.kr>
2024-10-21 04:26:42 +0900

    SOL-45 <LobbyScene -  포톤 서버에 준비 상태가 반영 되지 않는 현상>

--- Assets/Scripts/LobbyScene/UIManager_Lobby.cs
+++ Assets/Scripts/LobbyScene/UIManager_Lobby.cs
@@ -7,6 +7,12 @@ public class UIManager_Lobby : MonoBehaviour
 {
     public TextMeshProUGUI readyGuideText;
 
+
+    private void Start()
+    {
+        readyGuideText.text = "Press F to ready";
+    }
+
     private void OnEnable()
     {
         LobbyManager.OnChangedReady += ChangeText;
