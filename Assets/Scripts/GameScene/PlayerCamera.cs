07b850730189478d4bf15ad696eb130cdf1d6b28
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2024-10-22 14:13:27 +0900

    SOL-46 <Player이동 Photon연결>

--- Assets/Scripts/GameScene/PlayerCamera.cs
+++ Assets/Scripts/GameScene/PlayerCamera.cs
@@ -1,30 +1,39 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 using Cinemachine;
 
 public class PlayerCamera : MonoBehaviour
 {
     public Cinemachine.AxisState xAxis, yAxis;
+    private PhotonView _photonView;
 
     [SerializeField] Transform camFollowPos;
+
     void Start()
     {
-        
+        _photonView = GetComponent<PhotonView>();
     }
-    
+
     void Update()
     {
-        xAxis.Update(Time.deltaTime);
-        yAxis.Update(Time.deltaTime);
+        if (_photonView.IsMine)
+        {
+
+            xAxis.Update(Time.deltaTime);
+            yAxis.Update(Time.deltaTime);
+        }
     }
 
     private void LateUpdate()
     {
-        camFollowPos.localEulerAngles =
-            new Vector3(yAxis.Value, camFollowPos.localEulerAngles.y, camFollowPos.localEulerAngles.z);
-        transform.eulerAngles = new Vector3(transform.eulerAngles.x, xAxis.Value, transform.eulerAngles.z);
-
+        if (_photonView.IsMine)
+        {
+            camFollowPos.localEulerAngles =
+                new Vector3(yAxis.Value, camFollowPos.localEulerAngles.y, camFollowPos.localEulerAngles.z);
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, xAxis.Value, transform.eulerAngles.z);
+        }
     }
 }
