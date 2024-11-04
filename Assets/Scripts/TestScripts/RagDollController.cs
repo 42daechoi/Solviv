28b4f80ff84a1852aa03f5a9bee525b63c18cd69
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2024-11-05 02:07:28 +0900

    SOL-51<애니메이션 설정-5>

---
+++ Assets/Scripts/TestScripts/RagDollController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagDollController : MonoBehaviour
+{
+    public float speed;
+    public float strafeSpeed;
+    public float jumpForce;
+
+    public Rigidbody hips;
+    public bool isGrounded;
+    
+    void Start()
+    {
+        hips = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                hips.AddForce(hips.transform.forward * speed * 1.5f);    
+            }
+            else
+            {
+                hips.AddForce(hips.transform.forward * speed);
+            }
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            hips.AddForce(-hips.transform.right * strafeSpeed);    
+        }
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+            hips.AddForce(-hips.transform.forward * speed);    
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            hips.AddForce(hips.transform.right * strafeSpeed);    
+        }
+    }
+}
