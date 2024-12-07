60a3e101224a42bff49b5cbeafc21cddc42d5c3e
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2024-12-07 13:34:11 +0900

    SOL-69 <Setup FPS Camera for CowBoy Rio && Photon>

--- Assets/Scripts/GameScene/Movement.cs
+++ Assets/Scripts/GameScene/Movement.cs
@@ -1,33 +1,42 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun; // Photon 추가
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-
     public float walkSpeed = 4f;
     public float maxVelocityChange = 10f;
 
     private Vector2 input;
-
     private Rigidbody rb;
+    private PhotonView photonView; // PhotonView 추가
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        photonView = GetComponent<PhotonView>(); // PhotonView 컴포넌트 가져오기
     }
 
     // Update is called once per frame
     void Update()
     {
-        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        input.Normalize();
+        // 로컬 플레이어만 입력 처리
+        if (photonView.IsMine)
+        {
+            input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            input.Normalize();
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.AddForce(CalculateMovement(walkSpeed), ForceMode.VelocityChange);
+        // 로컬 플레이어만 움직임 처리
+        if (photonView.IsMine)
+        {
+            rb.AddForce(CalculateMovement(walkSpeed), ForceMode.VelocityChange);
+        }
     }
 
     Vector3 CalculateMovement(float _speed)
@@ -47,11 +56,11 @@ public class Movement : MonoBehaviour
 
             velocityChange.y = 0;
 
-            return (velocityChange);
+            return velocityChange;
         }
         else
         {
-            return new Vector3();
+            return Vector3.zero;
         }
     }
 }
