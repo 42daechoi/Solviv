b0d44cd14855edc180b4df8ffc1218a0e0410912
LeeGamza <135112596+LeeGamza@users.noreply.github.com>
2024-12-07 14:06:54 +0900

    SOL-70 <플레이어 달리기 및 점프 구현-1>
    콜라이더의 세분화 필요

--- Assets/Scripts/GameScene/Movement.cs
+++ Assets/Scripts/GameScene/Movement.cs
@@ -6,12 +6,19 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float walkSpeed = 4f;
+    public float sprintSpeed = 14f;
     public float maxVelocityChange = 10f;
-
+    [Space] public float jumpHeight = 5f;
+    
     private Vector2 input;
     private Rigidbody rb;
-    private PhotonView photonView; // PhotonView 추가
+    private PhotonView photonView; // PhotonView 추가 
 
+    private bool sprinting;
+
+    private bool jumping;
+
+    private bool grounded = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,6 +35,9 @@ public class Movement : MonoBehaviour
             input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
             input.Normalize();
         }
+
+        sprinting = Input.GetButton("Sprint");
+        jumping = Input.GetButton("Jump");
     }
 
     private void FixedUpdate()
@@ -35,7 +45,26 @@ public class Movement : MonoBehaviour
         // 로컬 플레이어만 움직임 처리
         if (photonView.IsMine)
         {
-            rb.AddForce(CalculateMovement(walkSpeed), ForceMode.VelocityChange);
+            if (grounded)
+            {
+                if (jumping)
+                {
+                    rb.velocity = new Vector3(rb.velocity.x, jumpHeight, rb.velocity.z);
+                }
+                else if (input.magnitude > 0.5f)
+                {
+                    rb.AddForce(CalculateMovement(sprinting ? sprintSpeed : walkSpeed), ForceMode.VelocityChange);
+                }
+                else
+                {
+                    var velocity1 = rb.velocity;
+                    velocity1 = new Vector3(velocity1.x * 0.2f * Time.fixedDeltaTime, velocity1.y,
+                        velocity1.z * 0.2f * Time.fixedDeltaTime);
+                    rb.velocity = velocity1;
+                }
+            }
+
+            grounded = false;
         }
     }
 
@@ -62,5 +91,10 @@ public class Movement : MonoBehaviour
         {
             return Vector3.zero;
         }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        grounded = true;
     }
 }
