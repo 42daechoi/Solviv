94f89cabb1dc035a0b503535a2b3c7f857a539d0
42 <daechoi@student.42seoul.kr>
2025-01-07 06:25:00 +0900

    SOL-90 <GetComponent 최적화>

--- Assets/Scripts/GameScene/Player/HeldItem.cs
+++ Assets/Scripts/GameScene/Player/HeldItem.cs
@@ -18,18 +18,20 @@ public class HeldItem : MonoBehaviour
 
     public void SelectItem(int keyCode)
     {
-        Inventory inventory = GetComponent<Inventory>();
-        Item[] itemSlots = inventory.itemSlots;
+        if (TryGetComponent(out Inventory inventory))
+        {
+            Item[] itemSlots = inventory.itemSlots;
 
-        if (keyCode == 1)
-        {
-            item = null;
-            // 맨손 드는 애니메이션 추가
-        }
-        else
-        {
-            item = itemSlots[keyCode - 2];
-            // 아이템에 맞는 애니메이션 추가
+            if (keyCode == 1)
+            {
+                item = null;
+                // 맨손 드는 애니메이션 추가
+            }
+            else
+            {
+                item = itemSlots[keyCode - 2];
+                // 아이템에 맞는 애니메이션 추가
+            }
         }
     }
 
